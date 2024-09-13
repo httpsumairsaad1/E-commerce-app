a5484beb38a626555f73c4667d73b6b87e84171e
Umair Saad <umairsaad52@gmail.com>
2024-09-13 10:35:05 -0700

    Final

--- App.jsx
+++ App.jsx
@@ -73,6 +73,7 @@ const App = () => {
               },
             }}
           />
+          {/* working */}
           <Tab.Screen
             name="REORDER"
             component={ReorderScreen}
